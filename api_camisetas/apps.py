ad4087c177d1a7b773e110063487700fa9479463
DavidGattuso <david_gatuso@hotmail.com>
2025-07-12 00:01:35 -0400

    The api_camisetas app was removed because the project now uses an external FastAPI for t-shirt data

--- api_camisetas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiCamisetasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_camisetas'
--- api_camisetas/urls.py
+++
@@ -1,7 +0,0 @@
-# api_camisetas/urls.py
-from django.urls import path
-from .views import ProductoListView
-
-urlpatterns = [
-    path('productos/', ProductoListView.as_view(), name='productos_list'),  # Vista para listar los productos
-]
